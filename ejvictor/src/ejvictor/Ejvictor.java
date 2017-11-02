83a8771a347d76e2396f35aab42d3edb1246409c
mrojas <mrojas@ultrabook-i5>
2017-11-01 19:09:58 -0500

    version 1 victor

---
+++ ejvictor/src/ejvictor/Ejvictor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejvictor;
+
+/**
+ *
+ * @author mrojas
+ */
+public class Ejvictor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ver 1");
+    }
+    
+}
